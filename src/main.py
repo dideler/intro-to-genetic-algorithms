1f6cb706d16153814ff5821998f0fb9216357573
Dennis Ideler <ideler.dennis@gmail.com>
2014-06-09 01:50:48 -0400

    Build interface w/ docopt and add POD

--- src/main.py
+++ src/main.py
@@ -1,4 +1,33 @@
 #!/usr/bin/env python
 
-if __name__ == "__main__":
-    run()
+"""Simple Genetic Algorithm for solving TSP.
+
+Usage:
+  main.py FILE --pop-size=POP_SIZE --max-gen=MAX_GENERATIONS --xover-rate=CROSSOVER_RATE --mute-rate=MUTATION_RATE --num-elites=NUM_ELITES --tournament-k=K
+  main.py (-h | --help)
+  main.py (-v | --version)
+
+Options:
+  -h --help     Show this screen.
+  -v --version  Show version.
+
+"""
+from docopt import docopt
+
+class Genotype(object):
+    """The population of chromosomes."""
+
+    def __init__(self, population, elite):
+        self.population = population
+        # TODO: new_pop?
+        self.elite = elite
+
+if __name__ == '__main__':
+    arguments = docopt(__doc__, version='0.1')
+    print(arguments)
+    # TODO
+    # read file
+    # initialize pop
+    # run ga (break down)
+
+# Default crossover is PMX. Can also use CX.
